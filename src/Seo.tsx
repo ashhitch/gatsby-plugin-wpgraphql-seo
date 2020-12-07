5cf47bfbad4fdc033775ecc4d0c4856911373d39
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2020-12-07 16:59:37 +0000

    refactor for raw schema

--- src/Seo.tsx
+++ src/Seo.tsx
@@ -7,31 +7,14 @@ interface SeoProps {
     title?: String;
     meta?: [];
     post?: any;
-    uri?: null;
-}
-
-interface IOrgSchema {
-    [x: string]: any;
-    '@type': string | string[];
-    '@id': string;
-    name: any;
-    url: any;
-    sameAs: any[];
-}
-
-interface IWebsiteSchema {
-    '@type': string;
-    '@id': string;
-    url: any;
-    name: any;
-    description: any;
-    publisher: {
-        '@id': string;
-    };
-    inLanguage: String;
+    postSchema?: IPageSchema;
 }
 
 interface IPageSchema {
+    '@context': string;
+    '@graph': IPageSchemaItems[];
+}
+interface IPageSchemaItems {
     '@type': any;
     '@id': string;
     url: string;
@@ -49,7 +32,7 @@ interface IPageSchema {
     dateModified?: string;
 }
 
-const SEO: FC<SeoProps> = ({ post = {}, meta = [], title, uri }) => {
+const SEO: FC<SeoProps> = ({ post = {}, meta = [], title, postSchema }) => {
     const { seo } = post;
 
     const { global } = useContext(SEOContext);
@@ -59,7 +42,7 @@ const SEO: FC<SeoProps> = ({ post = {}, meta = [], title, uri }) => {
     const webmaster = global?.webmaster;
     const social = global?.social;
 
-    const verification = [];
+    const verification: any[] = [];
 
     if (webmaster?.baiduVerify) {
         verification.push({
@@ -94,98 +77,6 @@ const SEO: FC<SeoProps> = ({ post = {}, meta = [], title, uri }) => {
 
     const metaTitle = title || seo.title;
     const metaDescription = seo?.metaDesc ? seo.metaDesc : '';
-
-    const pageUrl = seo?.canonical || `${schema?.siteUrl}${post?.uri || uri}`;
-
-    // const logo = schema.logo && schema.logo.localFile.childImageSharp.fixed;
-
-    const sameAs = social
-        ? Object.entries(social)
-              .map(([account, { url, username }]) => {
-                  if (username || url) {
-                      return username && account === 'twitter' ? `https://www.twitter.com/${username}` : url;
-                  }
-                  return null;
-              })
-              .filter(acc => !!acc)
-        : [];
-
-    const logo = schema?.logo
-        ? {
-              '@type': 'ImageObject',
-              '@id': `${schema.siteUrl}/#${schema.companyOrPerson === 'person' ? 'personlogo' : 'logo'}`,
-              inLanguage,
-              url: `${schema.siteUrl}${schema.logo?.localFile?.childImageSharp?.fixed.src}`,
-              width: schema.logo?.localFile?.childImageSharp?.fixed.width,
-              height: schema.logo?.localFile?.childImageSharp?.fixed.height,
-              caption: schema.logo.altText,
-          }
-        : null;
-
-    const image = schema?.logo
-        ? {
-              '@id': `${schema.siteUrl}/#${schema.companyOrPerson === 'person' ? 'personlogo' : 'logo'}`,
-          }
-        : null;
-
-    const orgSchema: IOrgSchema | null = schema
-        ? {
-              '@type': schema.companyOrPerson === 'person' ? ['Person', 'Organization'] : 'Organization',
-              '@id': `${schema.siteUrl}/#${schema.companyOrPerson === 'person' ? '/schema/person' : 'organization'}`,
-              name: schema.siteName,
-              url: schema.siteUrl,
-              sameAs,
-              [schema.companyOrPerson === 'person' ? 'image' : 'logo']: logo,
-              [schema.companyOrPerson === 'person' ? 'logo' : 'image']: image,
-          }
-        : null;
-
-    const websiteSchema: Partial<IWebsiteSchema> = schema
-        ? {
-              '@type':
-                  seo?.schema?.articleType && seo.schema.articleType.length >= 1 ? seo.schema.articleType : 'WebSite',
-              '@id': `${schema.siteUrl}/#website`,
-              url: schema.siteUrl,
-              name: metaTitle,
-              description: metaDescription,
-              publisher: {
-                  '@id': `${schema.siteUrl}/#organization`,
-              },
-              inLanguage,
-          }
-        : {};
-
-    const pageSchema: Partial<IPageSchema> =
-        post?.seo && schema
-            ? {
-                  '@type': seo?.schema?.pageType ? seo.schema.pageType : ['WebPage'],
-                  '@id': `${pageUrl}#webpage`,
-                  url: `${pageUrl}`,
-                  name: seo.title,
-                  isPartOf: { '@id': `${schema?.siteUrl}/#website` },
-                  description: seo?.description,
-                  inLanguage,
-                  potentialAction: [
-                      {
-                          '@type': 'ReadAction',
-                          target: [pageUrl],
-                      },
-                  ],
-              }
-            : {};
-
-    if (pageSchema && post?.date) {
-        pageSchema.datePublished = post.date;
-    }
-    if (pageSchema && post?.modified) {
-        pageSchema.dateModified = post.modified;
-    }
-
-    const graph = [orgSchema, websiteSchema, pageSchema].filter(a => !!a);
-    const schemaObj = {
-        '@context': 'https://schema.org',
-        '@graph': graph,
-    };
 
     return (
         <Helmet
@@ -243,7 +134,7 @@ const SEO: FC<SeoProps> = ({ post = {}, meta = [], title, uri }) => {
                 .concat(meta, verification)}
             encodeSpecialCharacters={false}
         >
-            <script type="application/ld+json">{JSON.stringify(schemaObj, null, 2)}</script>
+            {postSchema && <script type="application/ld+json">{JSON.stringify({ ...postSchema }, null, 2)}</script>}
         </Helmet>
     );
 };
