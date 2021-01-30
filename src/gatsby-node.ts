ae52a152191181fc47de507d39b9e9cbbe0b625e
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2021-01-30 14:05:24 +0000

    general config

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,10 +1,7 @@
-export const pluginOptionsSchema = ({ Joi }) => {
-  return Joi.object({
-    rootQuery: Joi.string()
-      .description(`Root query for global Yoast Data.`),
-    key: Joi.string()
-    .default('Wp')
-      .description(`Wey uses when setting up WpGraphql e.g Wp`),
-
-  })
-}
+export const pluginOptionsSchema = ({ Joi }) =>
+    Joi.object({
+        rootQuery: Joi.string().description(`Root query for global Yoast Data.`),
+        key: Joi.string()
+            .default('Wp')
+            .description(`Wey uses when setting up WpGraphql e.g Wp`),
+    });
--- src/gatsby-ssr.tsx
+++ src/gatsby-ssr.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useStaticQuery, graphql } from 'gatsby';
 import SEOContext from './SeoContext';
 
 const SEOProvider = (element, pluginOptions) => {
-  const {
-    [pluginOptions.key]: { seo },
-} = useStaticQuery(graphql`
+    const {
+        [pluginOptions.key]: { seo },
+    } = useStaticQuery(graphql`
     query SiteInfoQuery {
     
     }
 `);
 
+    return <SEOContext.Provider value={{ global: seo }}>{element}</SEOContext.Provider>;
+};
 
-  return <SEOContext.Provider value={{ global: seo }}>{element}</SEOContext.Provider>
-}
+export const wrapRootElement = ({ element }, pluginOptions) => {
+    if (pluginOptions.rootQuery) {
+        return SEOProvider(element, pluginOptions);
+    }
 
-export const wrapRootElement = ({ element, props }, pluginOptions) => {
-
-  if (pluginOptions.rootQuery) {
-
-    return   SEOProvider(element, pluginOptions);
-  }
-
-  return element;
-}
+    return element;
+};
