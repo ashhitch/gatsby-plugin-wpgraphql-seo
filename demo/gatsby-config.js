dc6a0515b0b3a862a406f31a82af7312f38b0c67
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2023-01-06 23:14:21 +0000

    up~date demo app

--- demo/gatsby-config.js
+++ demo/gatsby-config.js
@@ -1,3 +1,15 @@
+require('dotenv').config({
+    path: `.env`,
+});
+
 module.exports = {
-    plugins: ['gatsby-plugin-wpgraphql-seo'],
+    plugins: [
+        `gatsby-plugin-wpgraphql-seo`,
+        {
+            resolve: `gatsby-source-wordpress`,
+            options: {
+                url: process.env.GATSBY_WORDPRESS_API_URL,
+            },
+        },
+    ],
 };
--- demo/src/pages/index.js
+++ demo/src/pages/index.js
@@ -3,16 +3,90 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 import SEO from 'gatsby-plugin-wpgraphql-seo';
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
+    console.log(data);
     return (
         <main>
             <header>
                 <h1>Yoast SEO</h1>
+                <pre>
+                    <code>{JSON.stringify(data.wpPost.seo, null, 2)}</code>
+                </pre>
             </header>
         </main>
     );
 };
 
-export const Head = () => <SEO />;
+export const Head = ({ data }) => <SEO post={data.wpPost} />;
 
 export default IndexPage;
+
+export const pageQuery = graphql`
+    query {
+        wpPost(id: { eq: "cG9zdDox" }) {
+            id
+            title
+            seo {
+                canonical
+                title
+                metaDesc
+                focuskw
+                metaRobotsNoindex
+                metaRobotsNofollow
+                opengraphAuthor
+                opengraphDescription
+                opengraphTitle
+                opengraphDescription
+                opengraphImage {
+                    altText
+                    sourceUrl
+                    srcSet
+                }
+                opengraphUrl
+                opengraphSiteName
+                opengraphPublishedTime
+                opengraphModifiedTime
+                twitterTitle
+                twitterDescription
+                twitterImage {
+                    altText
+                    sourceUrl
+                    srcSet
+                }
+                breadcrumbs {
+                    url
+                    text
+                }
+                cornerstone
+                schema {
+                    pageType
+                    articleType
+                    raw
+                }
+                readingTime
+                fullHead
+            }
+            author {
+                node {
+                    seo {
+                        metaDesc
+                        metaRobotsNofollow
+                        metaRobotsNoindex
+                        title
+                        social {
+                            youTube
+                            wikipedia
+                            twitter
+                            soundCloud
+                            pinterest
+                            mySpace
+                            linkedIn
+                            instagram
+                            facebook
+                        }
+                    }
+                }
+            }
+        }
+    }
+`;
